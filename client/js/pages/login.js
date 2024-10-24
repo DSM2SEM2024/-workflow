a6093df621fdfb0cc292d263e5c178ba953b255d
trinitydevs <trinitynascimento@gmail.com>
2024-10-24 19:27:56 -0300

    update homepage

--- client/js/pages/login.js
+++ client/js/pages/login.js
@@ -1,42 +1,42 @@
 export const Login = {
     template: `
         <main id="login" class="dinamic-content d-flex justify-content-evenly align-items-center flex-row">
-            <section id="left-content">
+            <div id="left-content">
                 <figure>
                     <img src="../images/logo-cps.png" alt="Logo Centro Paula Souza">
                 </figure>
-            </section>
+            </div>
 
-            <section id="middle-content">
-            </section>
+            <div id="middle-content">
+            </div>
 
             <section id="right-content">
                 <h1>Login</h1>
                 <p class="subtitle">Faça login na plataforma para criar projetos</p>
 
-                <section>
+                <div>
                     <div class="d-flex flex-column justify-content-start form-inputs">
                         <input type="email" v-model="email" placeholder="E-mail">
                         <input type="password" v-model="password" placeholder="Senha">
                     </div>
 
-                    <section class="d-flex justify-content-between flex-row options-password">
+                    <div class="d-flex justify-content-between flex-row options-password">
                         <div class="d-flex justify-content-start align-items-center gap-2">
                             <input type="checkbox">
                             <label>Lembrar credenciais</label>
                         </div>
 
                         <a href="">Esqueci a senha</a>
-                    </section>
+                    </div>
 
-                    <section class="d-flex justify-content-center flex-column gap-3 options-login">
+                    <div class="d-flex justify-content-center flex-column gap-3 options-login">
                         <button type="submit" class="button btn-red">Entrar</button>
                         <button type="submit" class="d-flex justify-content-center flex-row align-items-center gap-3 button btn-white">
                             <img class="icon" src="../images/logo-microsoft.png" alt="Logo Microsoft">
                             Entrar com Microsoft
                         </button>
-                    </section>
-                </section>
+                    </div>
+                </div>
             </section>
         </main>
         {{email}} {{password}}
