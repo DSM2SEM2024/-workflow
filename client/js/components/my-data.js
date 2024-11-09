f8357321381d4f9ddd9945f448d9081ba165a69e
trinitydevs <trinitynascimento@gmail.com>
2024-11-08 23:22:37 -0300

    finished component

--- client/js/components/my-data.js
+++ client/js/components/my-data.js
@@ -2,57 +2,92 @@ import { navigate } from "../functions/navigate.js";
 
 export const MyData = {
     template: `
-    <section id="my-data">
+    <section v-show="showMyData" id="my-data" class="d-flex justify-content-evenly align-items-center flex-row">
+        <div class="dinamic-content">
             <div class="form-content">
-                <div class="section-title d-flex justify-content-between flex-row align-items-center"> 
-                    <h3>Minhas informações</h3>
-                    <img class="icon" src="../images/bottom-section.png" alt="Expandir">
-                </div>
-            </div>
-
-            <form>
-                <div class="form-inputs d-flex justify-content-start d-column">
-                    <input type="text" name="name" placeholder="Nome">
-                    <input type="email" name="email" placeholder="E-mail">
-                </div>
-
-                <div class="form-inputs d-flex justify-content-start d-column">
-                    <input type="text" name="expertise" placeholder="Formação">
-                </div>
-
-                <div class="form-inputs d-flex justify-content-start d-column">                               
-                    <div class="units">
-                    <!-- Insira um foreach com todas as unidades das quais o professor faz parte -->
-                        <li v-for="unity in units">{{unity}}</li>
-                    </div>    
-                    <!-- Insira um foreach com todos os cursos dos quais o professor ministra -->
-                    <div class="curses">
-                        <li v-for="curse in curses">{{curse}}</li>
-                    </div>    
-                </div>
-
-                <div class="section-password">
-                    <p>Clique no botão abaixo para alterar a sua senha de acesso.</p>
-                    <div class="form-inputs d-flex justify-content-start d-column">
-                    <!-- Deixe a senha do usuário setada aqui, quando ele clicar no botão de alterar a senha, limpe o campo -->
-                        <input type="password" name="password" placeholder="Digite a sua senha...">
-                        <input type="password" name="confirm-password" placeholder="Confirme a sua senha...">
+                <form>
+                    <!-- Deixe todas as informações do usuário já carregadas nos inputs. -->
+                    <div class="section-title d-flex justify-content-between flex-row align-items-center"> 
+                        <h3>Minhas informações</h3>
+                        <img class="icon" src="../images/bottom-section.png" alt="Expandir">
                     </div>
-                    <button @click="changePassword">Confirmar</button>
-                </div>
-                <hr>
-                <div class="form-footer d-flex justify-content-between d-row align-items-start">
-                    <button class="btn-create">Atualizar informações ‎ |
-                        <img class="icon" src="../images/next.png" alt="Expandir">
-                    </button>
-                </div>
+                                    
+                    <div class="form-inputs d-flex justify-content-start flex-row flex-wrap">
+                        <div class="d-flex flex-column align-items-start">
+                            <input type="text" name="name" placeholder="Nome" v-model="name">
+                            <p class="message-error error-name"></p>
+                        </div>
 
-                <button @click="updateMyData">Atualizar dados</button>
-            </form>
+                        <div class="d-flex flex-column align-items-start">
+                            <input type="email" name="email" placeholder="E-mail" v-model="email">
+                            <p class="message-error error-email"></p>
+                        </div>
+                    </div>
+
+                    <!-- O campo de formação só aparece se for professor (Reutilização da atualização de dados pessoais para coordenador e professor) -->
+                    <div class="form-inputs d-flex justify-content-start align-items-start flex-row flex-wrap">
+                        <div v-if="typeUser === 'teacher'" class="d-flex flex-column align-items-start">                            
+                            <input type="text" name="expertise" placeholder="Formação" v-model="expertise">
+                            <p class="message-error error-expertise">Mensagem de erro</p>
+                        </div>
+                        <div class="form-inputs d-flex justify-content-start align-items-start flex-row">
+                            <div class="d-flex flex-column align-items-start">                            
+                                <input type="password" name="password" placeholder="Digite a sua senha..." v-model="password">
+                                <p class="message-error error-password">Teste mensagem de erro</p>
+                            </div>
+                        </div>                        
+                            <button class="btn-change" @click="changePassword">Alterar senha</button>
+                    </div>
+
+                    <div class="section-list d-flex justify-content-start flex-row gap-4">                               
+                        <div class="units d-flex flex-column align-items-start">
+                            <!-- Insira um foreach com todas as unidades das quais o professor faz parte (só visualização) -->
+                            <h4>Unidades:</h4>
+                            <ul>
+                                <li v-for="unity in units">{{unity}}</li>
+                                <li>Fatec Mauá</li>
+                                <li>Fatec Mogi das Cruzes</li>
+                                <li>Fatec Guaratinguetá</li>
+                            </ul>
+                        </div>    
+                        <!-- Insira um foreach com todos os cursos dos quais o professor ministra (só visualização) -->
+                        <div class="curses d-flex flex-column align-items-start">
+                            <h4>Cursos:</h4>
+                                
+                            <ul>
+                                <li v-for="curse in curses">{{curse}}</li>
+                                <li>Design Digital</li>
+                                <li>Desenvolvimento de Sistemas</li>
+                                <li>Desenvolvimento Web</li>
+                            </ul>
+                        </div>    
+                    </div>
+                        
+                    <hr class="line-password">
+
+                    <div class="form-footer d-flex justify-content-between d-row align-items-start">
+                        <button click="updateMyData" class="btn-create">Atualizar informações ‎ |
+                            <img class="icon" src="../images/next.png" alt="Expandir">
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </section>
-
     `,
     data() {
+        //Trazer o tipo de usuário de acordo com a sessão para que eu possa manipular campos no front-end. Alguns campos devem ser ocultos para os outros tipos de usuário.
+        return {
+            showMyData: false,
+            typeUser: 'teacher',
+
+            name: 'Professor',
+            email: 'professor@fatec.sp.gov.br',
+            expertise: 'Graduado em Engenharia da Computação',
+            password: 'as547d!d9f',
+            // units: ['Fatec Mauá', 'Fatec Mogi das Cruzes', 'Fatec Guaratinguetá'],
+            // curses: ['Design Digital', 'Desenvolvimento de Sistemas', 'Desenvolvimento Web']
+        }
     },
     inject: ['urlBase'],
     methods: {
