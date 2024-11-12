788489fc905a2058741d92c27f49005ce3e68548
trinitydevs <trinitynascimento@gmail.com>
2024-11-12 19:47:57 -0300

    finish page management curses

--- client/js/pages/management-curses.js
+++ client/js/pages/management-curses.js
@@ -1,27 +1,26 @@
 import { validateAccess } from "../functions/validate-access.js";
 import { backend_url } from "../global-var/backend-url.js";
 
-export const ManagementTeachers = {
+export const ManagementCurses = {
     template: `
-    <main id="management-teachers" class="d-flex justify-content-evenly align-items-center flex-row">
+    <main id="management-curses" class="d-flex justify-content-evenly align-items-center flex-row">
         <section class="dinamic-content">
             <div class="page-section d-flex justify-content-start align-items-center">
-                <h2>Gerenciar Professores</h2>
+                <h2>Gerenciar Cursos</h2>
             </div>
             <div class="page-content d-flex justify-content-start align-items-start flex-column">
-                <div class="new-teacher-card">
-                    <h1>Adicione um novo professor à unidade</h1>
-                    <h2>Gerencie os Docentes através da central de gerenciamento. Crie e atualize dados existentes.</h2>
+                <div class="new-curse-card">
+                    <h1>Adicione um novo curso à unidade</h1>
+                    <h2>Gerencie os Cursos através da central de gerenciamento. Crie e atualize um curso existente.</h2>
                     <button class="btn-red" @click="">+ Criar Professor</button>
                 </div>
                 <div class="page-section d-flex justify-content-start align-items-center">
-                    <h2>Meus docentes</h2>
+                    <h2>Todos os cursos</h2>
                 </div>
-                <div id="teachers-container" class="d-flex justify-content-start align-items-start flex-column">
-                    <div class="teacher d-flex flex-column">
-                        <p class="teacher-unit">Fatec Itaquera - Prof° Miguel Reale</p>
-                        <p class="teacher-name">Fretz Sievers Junior</p>
-                        <p class="teacher-curse">Desenvolvimento de Software e Multiplataforma</p>
+                <div id="curses-container" class="d-flex justify-content-start align-items-start flex-column">
+                    <div class="curse d-flex flex-column">
+                        <p class="name-unit">Fatec Itaquera - Prof° Miguel Reale</p>
+                        <p class="curse-name">Desenvolvimento de Software e Multiplataforma</p>
                         <button class="btn-editar btn-red"><img class="icon" src="../images/lapis.png">Editar</button>
                     </div>
                 </div>
@@ -31,7 +30,7 @@ export const ManagementTeachers = {
 `,
     data() {
         return {
-            teachers: [],
+            curses: [],
             isProfessor: false,
             base_host: window.location.href.split('#')[0]
         };
